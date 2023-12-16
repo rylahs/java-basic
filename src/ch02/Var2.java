089392eac7def3855a66585bf1f5cbef981da951
Rylah <minsonyo@naver.com>
2023-12-16 10:33:18 +0900

    Ch02. Variable Start

---
+++ src/ch02/Var2.java
@@ -0,0 +1,18 @@
+package ch02;
+
+public class Var2 {
+    public static void main(String[] args) {
+        int c; // 변수 선언
+        c = 20; // 변수 초기화
+        int a = 100;
+
+        System.out.println(a);
+        System.out.println(a);
+
+        a = 250;
+        System.out.println(a);
+
+        System.out.println(c);
+
+    }
+}
