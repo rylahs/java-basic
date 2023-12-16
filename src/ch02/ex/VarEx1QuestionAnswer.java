96d0f64c99872b8fa1bd9e4723307e38177ba199
Rylah <minsonyo@naver.com>
2023-12-16 13:27:59 +0900

    Ch02. Variable Problem Solve

---
+++ src/ch02/ex/VarEx1QuestionAnswer.java
@@ -0,0 +1,10 @@
+package ch02.ex;
+
+public class VarEx1QuestionAnswer {
+    public static void main(String[] args) {
+        int num1 = 4, num2 = 3;
+        System.out.println(num1 + num2);
+        System.out.println(num1 - num2);
+        System.out.println(num1 * num2);
+    }
+}
---
+++ src/ch02/ex/VarEx3.java
@@ -0,0 +1,16 @@
+package ch02.ex;
+
+public class VarEx3 {
+    // 문제 3.
+    // long 타입의 변수를 선언하고, 그 변수를 10000000000(백억)으로 초기화 한 후 출력하는 프로그램을 작성하세요
+    // boolean 타입의 변수를 선언하고, 그 변수를 true로 초기화 한 후 출력하는 프로그램을 작성하세요.
+
+    public static void main(String[] args) {
+        long l = 10000000000L;
+        boolean b = true;
+        System.out.println(l);
+        System.out.println(b);
+
+    }
+
+}
