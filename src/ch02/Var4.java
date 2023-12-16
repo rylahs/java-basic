ed333d44df92d5b537613b7029cce59c30f4169f
Rylah <minsonyo@naver.com>
2023-12-16 12:44:19 +0900

    Ch02. Variable Init

---
+++ src/ch02/Var4.java
@@ -0,0 +1,12 @@
+package ch02;
+
+public class Var4 {
+    public static void main(String[] args) {
+        int a;
+        int b;
+
+        int c, d;
+
+
+    }
+}
---
+++ src/ch02/Var6.java
@@ -0,0 +1,9 @@
+package ch02;
+
+public class Var6 {
+    public static void main(String[] args) {
+        int a;
+//        System.out.println(a);
+
+    }
+}
