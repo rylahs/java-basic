2a2b1f4818753bdc9c6c4448a9a6a6f305f84436
Rylah <minsonyo@naver.com>
2023-12-19 23:01:06 +0900

    Ch04. Loop DoWhile

---
+++ src/ch05/loop/DoWhile1.java
@@ -0,0 +1,11 @@
+package ch05.loop;
+
+public class DoWhile1 {
+    public static void main(String[] args) {
+        int i = 10;
+        while ( i < 3 ) {
+            System.out.println("현재 숫자는:" + i);
+            i++;
+        }
+    }
+}
