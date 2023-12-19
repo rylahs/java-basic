283df60925838a1e9cb2d70dbd89e40a42caa664
Rylah <minsonyo@naver.com>
2023-12-19 19:37:21 +0900

    Ch03. Condition If(3) - Else if

---
+++ src/ch04/cond/If6.java
@@ -0,0 +1,32 @@
+package ch04.cond;
+
+public class If6 {
+    public static void main(String[] args) {
+        int price = 10000; // 아이템 가격
+        int age = 10; // 나이
+        int discount = 0;
+
+        if (price >= 10000)
+        {
+            discount += 1000;
+            System.out.println("10000원 이상 구매, 1000원 할인");
+        }
+
+        else if (age <= 10)
+        {
+            discount += 1000;
+            System.out.println("어린이 1000원 할인");
+        }
+        else
+        {
+            System.out.println("할인이 없어요");
+        }
+
+        System.out.println("총 할인 금액 : " + discount + "원");
+
+        if (true)
+            System.out.println("If문에서 실행됨");
+            System.out.println("If문에서 실행 안됨");
+
+    }
+}
