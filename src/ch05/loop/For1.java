c16a2403eb9bb98d2f76a634c7977af0ddeb5c65
Rylah <minsonyo@naver.com>
2023-12-19 23:20:46 +0900

    Ch04. Loop For(1)

---
+++ src/ch05/loop/For1.java
@@ -0,0 +1,9 @@
+package ch05.loop;
+
+public class For1 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i);
+        }
+    }
+}
