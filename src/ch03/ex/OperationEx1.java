5e7a95a984ded68f71774cd5dcb6fb74676ee1b2
Rylah <minsonyo@naver.com>
2023-12-19 13:54:36 +0900

    Ch03. Operator Problem

---
+++ src/ch03/ex/OperationEx1.java
@@ -0,0 +1,24 @@
+package ch03.ex;
+
+// 문제 1 - int와 평균
+// 다음과 같은 작업을 수행하는 프로그램을 작성하세요.
+// 클래스 이름은 OperationEx1 라고 적어주세요.
+
+// 1. num1, num2, num3 라는 이름의 세 개의 int 변수를 선언하고 각각 10, 20, 30으로 초기화하세요.
+// 2. 세 변수의 합을 계산하고, 그 결과를 sum이라는 이름의 int 변수에 저장하세요.
+// 3. 세 변수의 평균을 계산하고 그 결과를 average라는 이름의 int 변수에 저장하세요.
+//    평균 계산 시 소수점 이하의 결과는 버림하세요.
+// 4. sum과 average 변수의 값을 출력하세요.
+
+// 참고
+// java에서 int 끼리의 나눗 셈은 자동으로 소수점 이하를 버린다.
+public class OperationEx1 {
+    public static void main(String[] args) {
+        int num1 = 10, num2 = 20, num3 = 30;
+        int sum = num1 + num2 + num3;
+        int average = sum / 3;
+        System.out.println("sum = " + sum);
+        System.out.println("average = " + average);
+
+    }
+}
---
+++ src/ch03/ex/OperationEx3.java
@@ -0,0 +1,15 @@
+package ch03.ex;
+
+
+// 문제 3 - 합격 범위
+// 클래스 이름 : OperationEx3
+// int형 변수 score를 선언하세요.
+// score가 80점 이상이고, 100점 이하면 true를 출력하고 아니면 false를 출력하세요.
+
+public class OperationEx3 {
+    public static void main(String[] args) {
+        int score = 80;
+        boolean result = score >= 80 && score <= 100;
+        System.out.println("result = " + result);
+    }
+}
