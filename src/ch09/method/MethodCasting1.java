55dcf570f76d3dc851eaea55cf108b30a1f23a96
Rylah <minsonyo@naver.com>
2023-12-22 17:34:25 +0900

    Ch09. Method Casting

---
+++ src/ch09/method/MethodCasting1.java
@@ -0,0 +1,13 @@
+package ch09.method;
+
+public class MethodCasting1 {
+    public static void main(String[] args) {
+        double number = 1.5;
+//        printNumber(number);
+        printNumber((int) number);
+    }
+
+    public static void printNumber(int n) {
+        System.out.println("숫자 : " + n);
+    }
+}
